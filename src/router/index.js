b9497915a8c68fdf8f1ce3a80910497875c51d51
zhouzhishi@kedacom.com <zhouzhishi@kedacom.com>
2021-03-01 11:04:05 +0800

    清理垃圾文件

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,6 @@ import slot from '@/components/slot';
 import question from '@/components/question';
 
 import wangeditor from '@/components/wangeditor';
-import ueditor from '@/components/ueditor';
 
 // 把路由当成一个插件来使用
 // 模块化工程中 必须要用 Vue.use() 安装一下
@@ -86,11 +85,6 @@ export default new Router({
 			path: '/wangeditor', // 富文本编辑器
 			name: 'wangeditor',
 			component: wangeditor
-		},
-		{
-			path: '/ueditor', // 富文本编辑器
-			name: 'ueditor',
-			component: ueditor
 		}
 	]
 });
--- static/fileter.js
+++ static/fileter.js
@@ -1,6 +1,6 @@
 const filter1 = function (val, val1, val2) {
-  // if (val1 && val2) {
-  return val + '单位' + val1 + val2
-  // }
-}
-export default {filter1}
+	// if (val1 && val2) {
+	return val + '单位' + val1 + val2;
+	// }
+};
+export default {filter1};
